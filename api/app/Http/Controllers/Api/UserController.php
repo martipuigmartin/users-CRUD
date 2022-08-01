c59c91ef561e765949ba3c14d525758e9dfde9fb
Martí Puig <martipuigmartin@gmail.com>
2022-08-01 18:53:56 +0200

    first commit!

---
+++ api/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::create($request->all());
+        $user->save();
+    }
+
+    public function show($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+    }
+}
