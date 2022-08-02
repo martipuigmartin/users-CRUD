cd642c1bef6f72bb3ad0622393fac2fabca6822f
Martí Puig <martipuigmartin@gmail.com>
2022-08-02 20:50:22 +0200

    auth test done, some commenting pending

--- api/tests/Unit/UserTest.php
+++ api/tests/Unit/UserTest.php
@@ -3,48 +3,82 @@
 namespace Tests\Unit;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    /**
-     * This function tests the get request to the users route
-     */
     public function test_get_users()
     {
-        $this->get('/api/users')->assertStatus(200);
+        $user = User::factory()->create();
+        $user->save();
+        $this->actingAs($user)->get('/api/users')->assertStatus(200);
+        $user->delete();
     }
 
-    /**
-     * This function tests that the show user route returns a 200 status code
-     */
+    public function test_get_users_without_authentication()
+    {
+        $this->get('/api/users')->assertStatus(500);
+    }
+
     public function test_show_user()
     {
-        $user = User::first();
-        $this->get('/api/users/' . $user->id)->assertStatus(200);
+        $user = User::count() > 0 ? User::first() : User::factory()->create();
+
+        $this->actingAs($user)->get('/api/users/' . $user->id)->assertStatus(200);
+
+        if (User::count() == 1) {
+            $user->delete();
+        }
     }
 
-    /**
-     * We create a user, then we update it with a random email address, password and a name. We're asserting that the status code is 200
-     */
+    public function test_show_user_without_authentication()
+    {
+        $user = User::count() > 0 ? User::first() : User::factory()->create();
+
+        $this->get('/api/users/' . $user->id)->assertStatus(500);
+
+        if (User::count() == 1) {
+            $user->delete();
+        }
+    }
+
     public function test_update_user()
     {
         $user = User::factory()->create();
-        $this->put('/api/users/' . $user->id, [
+
+        $this->actingAs($user)->put('/api/users/' . $user->id, [
             'name' => 'Testing Update User',
             'email' => Str::random(10) . '@example.com',
             'password' => bcrypt(Str::random(10)),
         ])->assertStatus(200);
+
+        $user->delete();
     }
 
-    /**
-     * We create a user, then delete it
-     */
+    public function test_update_user_without_authentication()
+    {
+        $user = User::factory()->create();
+
+        $this->put('/api/users/' . $user->id, [
+            'name' => 'Testing Update User',
+            'email' => Str::random(10) . '@example.com',
+            'password' => bcrypt(Str::random(10)),
+        ])->assertStatus(500);
+
+        $user->delete();
+    }
+
     public function test_destroy_user()
     {
         $user = User::factory()->create();
-        $this->delete('/api/users/' . $user->id)->assertStatus(200);
+        $this->actingAs($user)->delete('/api/users/' . $user->id)->assertStatus(200);
+    }
+
+    public function test_destroy_user_without_test_update_user_without_authentication()
+    {
+        $user = User::factory()->create();
+        $this->delete('/api/users/' . $user->id)->assertStatus(500);
+        $user->delete();
     }
 }
